7a278b2e457de9db91ce47eaffa0802da8d9a44d
smatsumo2019 <smatsumo2019@falmouthschools.org>
2018-03-23 00:16:13 -0400

    Yay?

--- ScoutingApplicationMacPartition/app/src/main/java/com/example/scoutingapplicationmacpartition/CSVReader.java
+++ ScoutingApplicationMacPartition/app/src/main/java/com/example/scoutingapplicationmacpartition/CSVReader.java
@@ -1,22 +1,38 @@
 package com.example.scoutingapplicationmacpartition;
 
-//Definitions
-String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-String fileName = "savedData.csv";
-String filePath = baseDir + File.separator + fileName;
-File f = new File(filePath );
-CSVWriter writer;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CSVReader {
+    public void generateCSVFile(String[] values) throws IOException {
+    //Definitions
+    String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+    String fileName = "savedData.csv";
+    String filePath = baseDir + File.separator + fileName;
+    File f = new File(filePath);
+    CSVWriter writer;
 
 // Checking if everything listed above exists
-if(f.exists() && !f.isDirectory()){
-mFileWriter = new FileWriter(filePath , true);
-writer = new CSVWriter(mFileWriter);
-}
-else {
-writer = new CSVWriter(new FileWriter(filePath));
-}
-String[] data = {"matchString= " + values[0] + ", teamString= " + values[1] + ", autoScaleString= " + values[2] + ", autoSwitchString= " + values[3] + ", teleopScaleString= " + values[4] + ", teleopSwitchString= " + values[5] + ", teleopExchangeString= " + values[6] + ", competitionString= " + values[7]};
+if(f.exists()&&!f.isDirectory())
+
+    {
+        FileWriter mFileWriter = new FileWriter(filePath, true);
+        writer = new CSVWriter(mFileWriter);
+    }
+else
+
+    {
+        writer = new CSVWriter(new FileWriter(filePath));
+    }
+
+    String[] data = {"matchString= " + values[0] + ", teamString= " + values[1] + ", autoScaleString= " + values[2] + ", autoSwitchString= " + values[3] + ", teleopScaleString= " + values[4] + ", teleopSwitchString= " + values[5] + ", teleopExchangeString= " + values[6] + ", competitionString= " + values[7]};
 
 writer.writeNext(data);
 
 writer.close();
+
+}

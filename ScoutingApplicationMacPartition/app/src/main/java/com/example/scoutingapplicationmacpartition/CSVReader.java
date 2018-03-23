1bc5f388d03cda7d34bd35edb6fbb93c217c1ac9
Zerch97 <33588185+Zerch97@users.noreply.github.com>
2018-03-22 20:15:33 -0600

    Updated CSVReader to edit an specific file

--- ScoutingApplicationMacPartition/app/src/main/java/com/example/scoutingapplicationmacpartition/CSVReader.java
+++ ScoutingApplicationMacPartition/app/src/main/java/com/example/scoutingapplicationmacpartition/CSVReader.java
@@ -1,18 +1,27 @@
 package com.example.scoutingapplicationmacpartition;
 
-
+//important
+import com.opencsv.CSVReader;
+import java.io.FileReader;
 import java.io.IOException;
 
-public class CSVReader {
+//Start
+public class CSVReader extends DataEntryActivity {
 
-    public void readCSVFile(String[] args) throws IOException {
+    public static void main(String[] args) {
 
+        String csvFile = "./saves/savedData.csv";   //You must create a "saves" folder and a "savedData.csv" file first
 
-
-
-
-
-
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(csvFile));
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                System.out.println("Tablet [matchString= " + matchString[0] + ", teamString= " + teamString[1] + ", autoScaleString= " + autoScaleString[2] + ", autoSwitchString= " + autoSwitchString[3] + ", teleopScaleString= " + teleopScaleString[4] + ", teleopSwitchString= " + teleopSwitchString[5] + ", teleopExchangeString= " + teleopExchangeString[6] + ", competitionString= " + competitionString[7] "]");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }

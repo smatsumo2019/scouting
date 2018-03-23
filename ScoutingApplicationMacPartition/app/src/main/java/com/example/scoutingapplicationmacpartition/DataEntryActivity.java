100368ede442641b1a1b2fe2c07915884f70b50b
smatsumo2019 <smatsumo2019@falmouthschools.org>
2018-03-22 23:04:11 -0400

    Array Value Max... Changed!

--- ScoutingApplicationMacPartition/app/src/main/java/com/example/scoutingapplicationmacpartition/DataEntryActivity.java
+++ ScoutingApplicationMacPartition/app/src/main/java/com/example/scoutingapplicationmacpartition/DataEntryActivity.java
@@ -31,7 +31,7 @@ public class DataEntryActivity extends AppCompatActivity {
     String autoFoulString = "";
     String teleopFoulString = "";
 
-    String[] values = new String[11];
+    String[] values = new String[12];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
